16e1cbdaf5057a08711ae06e27d65d3fb301ca7e
DocWh0 <77495278+DocWh0@users.noreply.github.com>
2021-11-24 17:52:19 +0200

    new commit

--- app.js
+++ app.js
@@ -1124,3 +1124,82 @@ let value;
 value = Array.isArray(numArr);
 value - numArr[2];
 console.log(value, numArr);
+
+
+//Создать функцию которая принимает число N и возвращает массив заполненный числами от 1 до N.
+function getArray(num) {
+  const arr = [];
+  
+  for (let i = 1; i <= num; i++ ) {
+      arr.push(i);
+  }
+  return arr;
+  }
+  
+  console.log(getArray(10));
+
+
+
+//Создать функцию, которая принимает произвольное число массивов и удаляет из каждого массива первый елемент и возвращает массив из оставшихся значений. 
+function changeCollection() {
+  const res = [];
+    
+  for (let i = 0; i < arguments.length; i++) {
+      if (Array.isArray(arguments[i])) {
+          const el = arguments[i].slice();
+          el.shift();
+          res.push(el)
+      } 
+  }
+  return res;
+}
+
+console.log(changeCollection([1,2,3]))
+
+
+//Создать функцию, которая принимает массив пользователей, поле которое хочу проверить и значение на которое хочу проверить указанное поле. Проверять что все агрументы переданы. Если что-то не передано то возвращать обьект ошибки "return new ("Error mwssage")" Возвращать новый массив с пользователями соответствующие указанными параметрам.
+
+const users = [
+  {
+    "_id": "5e36b779dc76fe3db02adc32",
+    "balance": "$1,955.65",
+    "picture": "http://placehold.it/32x32",
+    "age": 33,
+    "name": "Berg Zimmerman",
+    "gender": "male"
+  },
+  {
+    "_id": "5e36b779d117774176f90e0b",
+    "balance": "$3,776.14",
+    "picture": "http://placehold.it/32x32",
+    "age": 37,
+    "name": "Deann Winters",
+    "gender": "female"
+  },
+  {
+    "_id": "5e36b779daf6e455ec54cf45",
+    "balance": "$3,424.84",
+    "picture": "http://placehold.it/32x32",
+    "age": 36,
+    "name": "Kari Waters",
+    "gender": "female"
+  }
+]
+
+
+function filterUsers(arr, key, value) {
+  if (!Array.isArray(arr)) return new Error('The first argument should be an array');
+  if (typeof key !== "string" || key === '') return new Error('The key should be a valid string');
+  if (value === undefined || value === null) return new Error('The value should be a valid value.');
+
+const res = [];
+
+for (i = 0; i < arr.length; i++) {
+  if (arr[i][key] === value) {
+    res.push(arr[i])
+  }
+}
+
+return res;
+}
+console.log(filterUsers(users, "age", 36))
